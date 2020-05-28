d8577661537d67dcec757e7cd787d07121b3503b
Nishant Bhaskar <bhaskarvandana9@gmail.com>
2020-05-28 00:12:04 -0700

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain
+{
+
+    //Creating connection to the database Step 0
+    public class Value
+    {
+        public int Id {get; set; }
+
+
+        public string Name {get; set;}
+    }
+}
---
+++ persistence/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistence
+{
+    public class DataContext: DbContext
+    {
+        //Creating connection to the database Step1
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+            
+                new Value {Id=1, Name = "Value101"},
+                new Value {Id=2, Name ="Nishant is King"},
+                new Value {Id=3, Name = "Nishant Bhaskar is everlasting quintillionire"}
+            );
+
+            
+
+        }
+
+    }
+}
